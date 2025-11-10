836b333e6774597856c69c90c9777dda4165d1bb
fabian matus <f.matus03@ufromail.cl>
2025-11-10 00:43:15 -0300

    feat: Creacion rutas para celular

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import LoginPhone from '../views/LoginPhone.vue'
+import HomePage from '../views/HomePage.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', name: 'Login', component: LoginPhone },
+  { path: '/register', name: 'Register', component: HomePage },
+  { path: '/home', name: 'Home', component: HomePage },
+  { path: '/incidentLog', name: 'IncidentLog', component: HomePage },
+  { path: '/myIncidents', name: 'MyIncidents', component: HomePage },
+  { path: '/incidentDetail', name: 'IncidentDetail', component: HomePage },
+  { path: '/profile', name: 'Profile', component: HomePage },
 ]
 
 const router = createRouter({
